be40578143af1512daa84510ab9fad60fd8735af
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-13 16:26:14 -0300

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(LoginPage());
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        appBar: CupertinoNavigationBar(),
+        ),
+      );
+  }
+}
